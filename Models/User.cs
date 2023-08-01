53c70c25647b8e7c4197067904878a0bf62e1c78
Kent Phan <kentphan2000@gmail.com>
2023-07-31 19:47:16 -0700

    intial commit

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace EatDeezApi.Models;
+
+public class User
+{
+	public long Id { get; set; }
+	public string? Name { get; set; }
+
+	public string? Email { get; set; }
+
+	public DateTime? DateOfBirth { get; set; }
+}
