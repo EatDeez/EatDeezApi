aa9a1df51888da729d5b58bc69cf2c9613e0fb7e
Kent Phan <kentphan2000@gmail.com>
2023-08-03 03:14:09 -0700

    Remove api route

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -4,7 +4,7 @@ using ZelpApi.Models;
 namespace ZelpApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("[controller]")]
 public class UserController : ControllerBase
 {
     // mock data
